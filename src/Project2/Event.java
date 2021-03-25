bd0a1383abbfaf2fcaff77cfcea4a5990b1f88e0
Louis <louis.pelletier@outlook.com>
2021-03-24 21:00:21 -0400

    Event + simulation "Done"

--- src/Project2/Event.java
+++ src/Project2/Event.java
@@ -5,68 +5,100 @@ import java.util.List;
 import java.util.Random;
 
 public class Event {
+    public static final int  NAISSANCE  = 1;
+    public static final int  REPRODUCTION  = 2;
+    public static final int  MORT  = 3;
     int fidelite = 90;
     public Sim subject;
     public float time;
+    public int type;
 
-    List<Sim> population = new ArrayList<>();
 
-    public void naissance(Sim newSim, float t){
-        subject = newSim;
-        time = t;
+
+    public Event(int type, Sim sim, float t) {
+        this.type= type;
+        this.time= t;
+        this.subject = sim;
+    }
+
+    public List<Event> naissance(List<Sim> population){
+        type = NAISSANCE;
+        List<Event> events = new ArrayList<>();
+        Event death;
+        Event reproduction = null;
 
         AgeModel am = new AgeModel();
         Random random = new Random();
 
-        //N1
+        //N1  on tire une durée de vie D au hasard – enfiler nouvel événement de mort pour x, à temps t+D.
         double rdmAge = am.randomAge(random);
-        subject.setDeathTime(rdmAge+t);
-        // enfiler nouvel événement de mort pour x, à temps t+D.
+        Sim s = subject;
+        s.setDeathTime(rdmAge+time);
+        death = new Event(3,s,(float) s.getDeathTime());
+
 
         //N2  si x est une fille, alors on tire un temps d’attente A jusqu’à reproduction – enfiler nouvel événement de reproduction pour x, à temps t+A.
         if(subject.getSex() == Sim.Sex.F){
-
+            double span = am.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
+            reproduction = new Event(2,s,(float)span + time);
         }
 
-        //N3 on enregistre x dans la population
-        population.add(subject);
+        //N3  on enregistre x dans la population
+        population.add(s);
 
+        //Ajouts events
+        events.add(death);
+        if(reproduction != null){
+            events.add(reproduction);
+        }
+
+        return events;
     }
     //Lors de la mort de sim x à temps  t, on le retire de la population.
-    public void mort(Sim sim){
-        population.remove(sim);
+    public void mort(List<Sim> population){
+        type = MORT;
+        population.remove(subject);
     }
 
-    public void reproduction(Sim mere, float t){
+    public Event reproduction(Sim mere, List<Sim> population){
+        type = REPRODUCTION;
+        Event e;
+        AgeModel am = new AgeModel();
+
+
         //R1 si x est morte, alors rien à faire
-        if (mere.getDeathTime() < t){
-            return;
+        if (mere.getDeathTime() < time){
+            return null;
         }
 
         //R2 si x est d’age de reproduction (sinon, juste passer à r3), alors choisir un partenaire y pour avoir un bébé avec
-        if(mere.isMatingAge(t)){
-            Sim pere = choisirPere(mere, t);
+        if(mere.isMatingAge(time)){
+            Sim pere = choisirPere(mere, population);
             //créer le sim bébé avec sexe au hasard, temps de naissance t, et enfiler l’événement de sa naissance
-            Sim s = new Sim(mere,pere,t,Sim.Sex.values()[new Random().nextInt(Sim.Sex.values().length)]);
-
+            Sim bebe = new Sim(mere,pere,time,Sim.Sex.values()[new Random().nextInt(Sim.Sex.values().length)]);
+            e = new Event(NAISSANCE,bebe,time);
         }
-        //R3 [r3] on tire un nouveau temps d’attente A jusqu’à reproduction –
-        // enfiler nouvel événement de reproduction pour x, à temps t+A (également si elle est d’âge ou non)
-
+        else {
+            //R3 [r3] on tire un nouveau temps d’attente A jusqu’à reproduction –
+            // enfiler nouvel événement de reproduction pour x, à temps t+A (également si elle est d’âge ou non)
+            double span = am.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
+            e = new Event(2,mere,(float)span + time);
+        }
+        return e;
     }
 
-    private Sim choisirPere(Sim x, float t) {
+    private Sim choisirPere(Sim x,List<Sim> population) {
         Random RND = new Random(); // générateur de nombres pseudoaléatoires
         Sim y = null; // choisir pere y
-        if (!x.isInARelationship(t) || RND.nextDouble()>fidelite)
+        if (!x.isInARelationship(time) || RND.nextDouble()>fidelite)
         { // partenaire au hasard
             do
             {
                 Sim z = population.get(new Random().nextInt(population.size()));
-                if (z.getSex()!=x.getSex() && z.isMatingAge(t)) // isMatingAge() vérifie si z est de l'age acceptable
+                if (z.getSex()!=x.getSex() && z.isMatingAge(time)) // isMatingAge() vérifie si z est de l'age acceptable
                 {
-                    if (x.isInARelationship(t) // z accepte si x est infidèle
-                            || !z.isInARelationship(t)
+                    if (x.isInARelationship(time) // z accepte si x est infidèle
+                            || !z.isInARelationship(time)
                             || RND.nextDouble()>fidelite)
                     {  y = z; x.setMate(y);}
                 }
--- src/Project2/Simulation.java
+++ src/Project2/Simulation.java
@@ -1,23 +1,41 @@
 package Project2;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Simulation {
+    public PQ eventQ;
+    public static final int  NAISSANCE  = 1;
+    public static final int  REPRODUCTION  = 2;
+    public static final int  MORT  = 3;
+    List<Sim> population = new ArrayList<>();
     public Simulation(int n, double Tmax){
-        PQ eventQ = new PQ(50); // file de priorité
+        eventQ = new PQ(50); // file de priorité
         for (int i=0; i<n; i++)
         {
-            Sim fondateur = new Sim(Sim.Sex.M); // sexe au hasard, naissance à 0.0
-            Event E = new Event();
-            E.naissance(fondateur,0);
+            Sim fondateur = new Sim(Sim.Sex.values()[new Random().nextInt(Sim.Sex.values().length)]); // sexe au hasard, naissance à 0.0
+            Event E = new Event(1,fondateur, 0);
             eventQ.insert(E); // insertion dans la file de priorité
         }
         while (!eventQ.isEmpty())
         {
             Event E = eventQ.deleteMin(); // prochain événement
             if (E.time > Tmax) break; // arrêter à Tmax
-            if (E.subject.getDeathTime() > E.time)
-            {
-               // traiter événement E
+            if(E.type == NAISSANCE){
+                List<Event> evs =  E.naissance(population);
+                for(Event ev : evs){
+                        Event e = new Event(ev.type,ev.subject,ev.time);
+                        eventQ.insert(e);
+                }
+            }
+            else if(E.type == REPRODUCTION){
+                Event ev = E.reproduction(E.subject,population);
+                eventQ.insert(ev);
+            }
+            else {
+                E.mort(population);
             }
         }
     }
